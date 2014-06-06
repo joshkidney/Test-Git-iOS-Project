f86a5124cd78d69e80b2dbeec6767cb54dc8179c
Joshua Kidney <meanredspider@Josh-Kidney-Old.local>
2014-06-06 05:25:49 -0500

    Initial Commit

---
+++ Git Test/CGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CGAppDelegate.h
+//  Git Test
+//
+//  Created by Joshua Kidney on 6/6/14.
+//  Copyright (c) 2014 Cyclops Gorilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CGViewController.m
@@ -0,0 +1,29 @@
+//
+//  CGViewController.m
+//  Git Test
+//
+//  Created by Joshua Kidney on 6/6/14.
+//  Copyright (c) 2014 Cyclops Gorilla. All rights reserved.
+//
+
+#import "CGViewController.h"
+
+@interface CGViewController ()
+
+@end
+
+@implementation CGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
